d70a0778a783bb76fd5bbe35b42e8992f13bd7cd
John Biffl <johnbiffl@gmail.com>
2024-02-22 11:04:58 -0500

    updated raoult_law_kvalue docstring formatting

--- src/raoult_law_kvalue.py
+++ src/raoult_law_kvalue.py
@@ -2,35 +2,37 @@ from src.antoine import antoine
 import numpy as np
 
 def raoult_law_kvalue( T, P, a, *gamma):
-    # Calculates the equilibrium coefficient from Raoult's law
-    # INPUTS:
-    #
-    #  T - temperature (units of K)
-    #
-    #  P - pressure (units of bar)
-    #
-    #  a - Antoine equation coefficients with coefficients for each species in
-    #      a row. When drawing Antoine coefficients from NIST, these will expect T
-    #      and p in units of K and bar. Antoine coefficients from other sources 
-    #      will require compatible units.
-    #
-    #  gamma - OPTIONAL activity coefficients for use in a modified Raoult's law.
-    #
-    #  tempUnit - The units of temperature. Can be Kelvin, Fahrenheit, Celsius, or Rankine
-    #             Parameter should be inputted as the first letter of the temperature scale.
-    #             
-    #
-    # OUTPUT:
-    #
-    #  K - row vector containing the K-value for each species
-    #
-    #  Example: (eg: raoult_law_kvalue(500, 2, [1,2,3], *gamma))        
-    #  
-    # Code originally by James C. Sutherland
-    # Modified by Tyler R. Josephson
+    """    
+    Calculates the equilibrium coefficient from Raoult's law
+    INPUTS:
+    
+     T - temperature (units of K)
+    
+     P - pressure (units of bar)
+    
+     a - Antoine equation coefficients with coefficients for each species in
+         a row. When drawing Antoine coefficients from NIST, these will expect T
+         and p in units of K and bar. Antoine coefficients from other sources 
+         will require compatible units.
+    
+     gamma - OPTIONAL activity coefficients for use in a modified Raoult's law.
+    
+     tempUnit - The units of temperature. Can be Kelvin, Fahrenheit, Celsius, or Rankine
+                Parameter should be inputted as the first letter of the temperature scale.
+                
+    
+    OUTPUT:
+    
+     K - row vector containing the K-value for each species
+    
+     Example: (eg: raoult_law_kvalue(500, 2, [1,2,3], *gamma))        
+     
+    Code originally by James C. Sutherland
+    Modified by Tyler R. Josephson
+    """
     
     ns,nc = a.shape
-    # makes np array of zeros
+    #makes np array of zeros
     K = np.zeros(ns) 
     Ps = antoine(a, T)  
     K = Ps/P
@@ -41,13 +43,13 @@ def raoult_law_kvalue( T, P, a, *gamma):
 
 
 
-# This is intended to test things to ensure that it is working properly.
+#This is intended to test things to ensure that it is working properly.
 if __name__ == '__main__':
     from scipy.optimize import newton
 
-    P = 101325  # Pressure in Pa
+    P = 101325  #Pressure in Pa
 
-    # We should update this example! Units of test not consistent with NIST
+    #We should update this example! Units of test not consistent with NIST
     propane = [6.80398, 803.810,  246.990]
     benzene = [6.90565, 1211.033, 220.79 ]
     antoineCoefs = np.array( [propane, benzene] )
